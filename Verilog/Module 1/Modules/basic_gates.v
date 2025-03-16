69f2963628e18423309685ad478c8c9130732065
A V Sohan Aiyappa <151933920+AV-Sohan-Aiyappa@users.noreply.github.com>
2025-03-16 21:37:50 +0530

    Create basic_gates.v

---
+++ Verilog/Module 1/Modules/basic_gates.v
@@ -0,0 +1,17 @@
+//basic_gates
+//
+// Basic Logic Gates
+
+
+
+module basic_gates(
+    input wire a, b,
+    output wire nand_out, and_out, or_out, not_out, xor_out, nor_out
+);
+    assign nand_out = ~(a & b);
+    assign and_out = a & b;
+    assign or_out = a | b;
+    assign not_out = ~a;
+    assign xor_out = a ^ b;
+    assign nor_out = ~(a | b);
+endmodule
