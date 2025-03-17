889568aebc3c3d66d8c59f82b02d59fd9575c965
A V Sohan Aiyappa <151933920+AV-Sohan-Aiyappa@users.noreply.github.com>
2025-03-17 18:55:47 +0530

    Update and rename Mux4x16.v to Mux4way16.v

--- Verilog/Module 1/Modules/Mux4way16.v
+++ Verilog/Module 1/Modules/Mux4way16.v
@@ -1,7 +1,7 @@
-// Mux4x16.v
+// Mux4way16.v
 // Created by Sohan on 17-03-2025
 
-module Mux4x16(input [15:0] a, b, c, d, input [1:0] sel, output [15:0] out);
+module Mux4way16(input [15:0] a, b, c, d, input [1:0] sel, output [15:0] out);
     assign out = (sel == 2'b00) ? a :
                  (sel == 2'b01) ? b :
                  (sel == 2'b10) ? c : d;
